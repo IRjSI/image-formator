fc522b8e5e125e704b5c5ad1b1478d11a148d11a
IRjSI <rajusuthar0077@gmail.com>
2025-06-15 00:24:58 +0530

    Add favicon and update title in RootLayout for improved branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Social Media Image Format Converter</title>
+        <link rel="icon" href="https://icones.pro/wp-content/uploads/2021/06/icone-d-image-bleue.png" />
+      </head>
       <body>
         {children}
       </body>
