b73387db49eb773be8a9ba85b732f1c877e006f0
IRjSI <rajusuthar0077@gmail.com>
2025-06-14 12:09:14 +0530

    Implement image upload functionality with Cloudinary integration and add social media format options

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,139 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
+import { CldImage } from 'next-cloudinary';
+import React, { useEffect, useRef, useState } from 'react'
+
+const socialFormats = {
+  "instagram square (1:1)": { width: 1080, height: 1080, aspectRatio: "1:1" },
+  "instagram portrait (4:5)": { width: 1080, height: 1350, aspectRatio: "4:5" },
+  "tweeter post (16:9)": { width: 1200, height: 675, aspectRatio: "16:9" },
+  "tweeter header (3:1)": { width: 1500, height: 500, aspectRatio: "3:1" },
+  "facebook cover (205:78)": { width: 820, height: 312, aspectRatio: "205:78" },
+}
+
+type SocialFormat = keyof typeof socialFormats;
+
+export default function SocialPage() {
+  const [uploadedImage, setUploadedImage] = useState<string | null>(null);
+  const [selectedFormat, setSelectedFormat] = useState<SocialFormat>("instagram square (1:1)");
+  const [isUploading, setIsUploading] = useState(false);
+  const [isTransforming, setIsTransforming] = useState(false);
+  const imageRef = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    if (uploadedImage) {
+      setIsTransforming(true);
+    }
+  }, [selectedFormat, uploadedImage])
+
+  const handleFileUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (!file) return;
+
+    setIsUploading(true);
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const response = await fetch("/api/image-upload", {
+        method: "POST",
+        body: formData
+      })
+      if (!response.ok) {
+        throw new Error("Failed to upload image")
+      }
+
+      const data = await response.json();
+      setUploadedImage(data.publicId);
+      
+      
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsUploading(false);
+      console.log(uploadedImage);
+    }
+  }
+
+  const handleDownload = async () => {
+    if (!imageRef.current) {      
+      return
+    }    
+
+    // to download image ***
+    fetch(imageRef.current.src)
+      .then(response => response.blob())
+      .then((blob) => {
+        const url = window.URL.createObjectURL(blob)
+        const link = document.createElement("a")
+        link.href = url
+        link.download = "image_transformed.jpg"
+        document.body.appendChild(link)
+        link.click()
+        document.body.removeChild(link)
+        window.URL.revokeObjectURL(url)
+      })
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className='w-full flex flex-col items-center min-h-screen bg-base-300 p-8'>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+      <span className="mb-2 mt-8 text-xl font-semibold">Social Media Image format converter</span>
+
+      <div className="bg-base-100 max-w-xl shadow-sm rounded-xl">
+        <div className="p-4 flex flex-col gap-4 items-center justify-center">
+          <label className="form-control w-full">
+            <span className=''>Select Image</span>
+            <input
+              type="file"
+              accept="image/*"
+              className="file-input file-input-bordered w-full"
+              onChange={handleFileUpload}
+              disabled={isUploading}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </label>
+          {isUploading && <span className="loading loading-spinner loading-md mx-auto" />}
+          {uploadedImage ? (
+            <div className="flex flex-col gap-4 items-center">
+              <select
+                className="select select-bordered w-full"
+                value={selectedFormat}
+                onChange={e => setSelectedFormat(e.target.value as SocialFormat)}
+                disabled={isTransforming}
+              >
+                {Object.keys(socialFormats).map(format => (
+                  <option key={format} value={format}>{format}</option>
+                ))}
+              </select>
+              <CldImage
+                width={socialFormats[selectedFormat].width}
+                height={socialFormats[selectedFormat].height}
+                src={uploadedImage}
+                sizes="100vw"
+                alt="transformed image"
+                crop="fill"
+                aspectRatio={socialFormats[selectedFormat].aspectRatio}
+                gravity='auto'
+                ref={imageRef}
+                onLoad={() => setIsTransforming(false)}
+              />
+              <button
+                className="btn btn-primary w-full"
+                onClick={handleDownload}
+                disabled={isTransforming}
+              >
+                Download
+              </button>
+              {isTransforming && <span className="text-gray-400 font-light">transforming...</span>}
+            </div>
+          ) : (
+            <div>
+              <p className='text-gray-400'>Select an image for format options</p>
+            </div>
+          )}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+
     </div>
-  );
+  )
 }
